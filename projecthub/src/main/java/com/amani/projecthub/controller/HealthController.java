220237dee7a600851ed9470a81c9bb75630e1fc3
Amani Shema <amanishema90@gmail.com>
2026-02-06 20:18:39 +0200

    feat: add health check controller

---
+++ projecthub/src/main/java/com/amani/projecthub/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.amani.projecthub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Project Hub is running 🚀";
+    }
+}
